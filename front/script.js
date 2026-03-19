a046ff4a71e95681e0b2eb252e0a46b6509895a0
Gkhosty <omarsaiidy007@gmail.com>
2026-03-19 11:48:06 +0100

    ajouté les projets

---
+++ front/script.js
@@ -0,0 +1,15 @@
+const hamburger = document.getElementById('hamburger');
+const navMenu = document.querySelector('.nav-menu');
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    navMenu.classList.toggle('open');
+});
+
+// Fermer le menu quand on clique sur un lien
+document.querySelectorAll('.nav-menu a').forEach(link => {
+    link.addEventListener('click', () => {
+        hamburger.classList.remove('active');
+        navMenu.classList.remove('open');
+    });
+});
